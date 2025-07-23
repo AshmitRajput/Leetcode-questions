db9d14e025ff444f09b4c34212595f0be8057116
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2025-07-23 15:44:32 +0530

    Time: 0 ms (100%), Space: 8.9 MB (53.89%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int count=0;
+        int count=0,start=0;
         string ans;
         for(int i=0;i<s.length();i++){
-           if(s[i]=='('){
-            if(count>0) ans += s[i];
-            count++;
-           }
-           else{
-            count--;
-            if(count>0) ans += s[i];
-           }
+           if (s[i] == '(') count++;
+            else count--;
+            if ( count == 0) {
+                ans += s.substr(start + 1, i - start - 1);
+                start = i + 1;
+            }
+
         }
         return ans;
     }
