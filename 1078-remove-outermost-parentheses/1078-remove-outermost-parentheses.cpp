44bef849993821ee0d5564899f7f7a46c6ee66eb
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2025-07-23 16:00:06 +0530

    Update 1078-remove-outermost-parentheses.cpp

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,3 +1,4 @@
+//Final optimized solution after looking at different approaches.
 class Solution {
 public:
     string removeOuterParentheses(string s) {
@@ -15,3 +16,47 @@ public:
         return ans;
     }
 };
+
+//2nd Attempt - optimized solution
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int count=0;
+        string ans;
+        for(int i=0;i<s.length();i++){
+           if(s[i]=='('){
+            if(count>0) ans += s[i];
+            count++;
+           }
+           else{
+            count--;
+            if(count>0) ans += s[i];
+           }
+        }
+        return ans;
+    }
+};
+
+
+//1st Attempt.
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int count=0;
+        string ans;
+        for(int i=0;i<s.length();i++){
+            if(count==0&&s[i]=='(') count++;
+            else if(s[i]=='('&&count>0){
+                count++;
+                ans += '(';
+            }
+            else if(s[i]==')'&&count>1){
+                count--;
+                ans += ')';
+            }
+            else if(s[i]==')'&& count==1) count--;
+        }
+        return ans;
+    }
+};
+
