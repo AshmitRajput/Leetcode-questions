d2dfc587e161956b0110855acc332e5bd656bf1d
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2025-07-28 19:32:51 +0530

    Time: 0 ms (100%), Space: 31.4 MB (45.34%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int start=0,end=nums.size()-1,ans = -1;
 
         while(start<=end){
-            int mid=(start+end)/2;
+            int mid=start+((end-start)/2);
             if(nums[mid]<target) start=mid+1;
             else if(nums[mid]>target) end=mid-1;
             else if(nums[mid]==target){
