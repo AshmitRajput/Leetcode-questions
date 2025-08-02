385c76ee494b61f1e627b95e569e27e12b8a5522
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2025-08-03 00:15:51 +0530

    Update 0540-single-element-in-a-sorted-array.cpp

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        for(int i=0;i<nums.size();i=i+2){
+            if(nums.size()==1) return nums[0];
+            else if(nums[i]!=nums[i+1] || i==nums.size()-1) return nums[i];
+        }
+        return -1;
+
+    }
+};
+
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
         int left = 0, right = nums.size() - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (mid % 2 == 1) {
-                mid--;
-            }
-            if (nums[mid] != nums[mid + 1]) {
-                right = mid;
-            } else {
+        while (left<right) {
+            int mid=(left+right)/2;
+            if (mid%2==1) mid--;
+            if (nums[mid]!=nums[mid+1]) right = mid; 
+            else {
                 left = mid + 2;
             }
         }
