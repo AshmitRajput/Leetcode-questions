31f23bc34315f013d449d5925c6fa7547f4555df
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2025-08-04 20:11:17 +0530

    Update 0242-valid-anagram.cpp

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,3 +1,5 @@
+
+//my approach
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -12,5 +14,24 @@ public:
         else{
             return false;
         }
+    }
+};
+
+//second solution after looking at other approaches.
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        map<char,int> smap;
+        map<char,int> tmap;
+        for(int i=0;i<s.size();i++){
+            smap[s[i]]++;tmap[t[i]]++;
+        }
+        for(auto &i:smap){
+            if(tmap[i.first]!=i.second){
+                return false;
+            }
+        }
+        return true;
     }
 };
