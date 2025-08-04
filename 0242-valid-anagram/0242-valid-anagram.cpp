4f9a1a595e2196873b5b8f95aed743dd1f71b8ab
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2025-08-04 20:09:17 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        map<char,int> smap;
-        map<char,int> tmap;
+        vector<int>arr1(26,0);
+        vector<int>arr2(26,0);
         for(int i=0;i<s.size();i++){
-            smap[s[i]]++;tmap[t[i]]++;
+            arr1[int(s[i])-97]++;
+            arr2[int(t[i])-97]++;
         }
-        for(auto &i:smap){
-            if(tmap[i.first]!=i.second){
-                return false;
-            }
+        if(arr1==arr2) return true;
+        else{
+            return false;
         }
-        return true;
     }
 };
