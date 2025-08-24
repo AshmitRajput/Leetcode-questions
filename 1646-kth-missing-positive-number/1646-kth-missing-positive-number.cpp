006c97c77ebabf776e3f5f886565d960bbb6a0f1
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2025-08-24 23:09:42 +0530

    Update 1646-kth-missing-positive-number.cpp

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -1,3 +1,21 @@
+//brute force, first approach
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        vector<int> arr2(2001,0);
+        for(int i=0;i<arr.size();i++){
+            arr2[arr[i]]=-1;
+        }
+        int count=0,i=1;
+        while(count!=k){
+            if(arr2[i]==0) count++;
+            i++;
+        }
+        return i-1;
+    }
+};
+
+//second attempt after looking at approaches
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
