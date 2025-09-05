ae95dff03e301acd0923949429b1fbd2efde01f6
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2025-09-05 11:05:09 +0530

    Time: 0 ms (100%), Space: 50.6 MB (29.24%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -1,60 +1,30 @@
 class Solution {
 public:
-    vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int ind1=0,ind2=0,row=mat.size(),col=mat[0].size();
-        int count=0;
-        while(count<4){
-            int ele=mat[ind1][ind2];
-            if(ind2+1<col){
-                if(mat[ind1][ind2+1]<ele){
-                    count++;
-                    if(count>=4) break;
-                }
-                else{
-                    ind2=ind2+1;
-                    count=0;
-                    ele=mat[ind1][ind2];
-                }
+    int MaxIndex(vector<vector<int>>& mat, int n,int m, int col){
+        int index = -1;
+        int maxvalue = -1;
+        for(int i=0;i<n;i++){
+            if(mat[i][col]>maxvalue){
+                maxvalue=mat[i][col];
+                index=i;
             }
-            else count++;
-            if(ind2-1>=0){
-                if(mat[ind1][ind2-1]<ele){
-                    count++;
-                    if(count>=4) break;
-                }
-                else{
-                    ind2=ind2-1;
-                    count=0;
-                    ele=mat[ind1][ind2];
-                }
-            }
-            else count++;
-            if(ind1+1<row){
-                if(mat[ind1+1][ind2]<ele){
-                    count++;
-                    if(count>=4) break;
-                }
-                else{
-                    ind1=ind1+1;
-                    count=0;
-                    ele=mat[ind1][ind2];
-                }
-            }
-            else count++;
-            if(ind1-1>=0){
-                if(mat[ind1-1][ind2]<ele){
-                    count++;
-                    if(count>=4) break;
-                }
-                else{
-                    ind1=ind1-1;
-                    count=0;
-                    ele=mat[ind1][ind2];
-                }
-            }
-            else count++;
         }
-        vector<int>ans{ind1,ind2};
-        return ans;
+        return index;
+    }
+
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int low=0,high=m-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int rowmax=MaxIndex(mat,n,m,mid);
+            int left=mid-1>=0 ? mat[rowmax][mid-1]:-1;
+            int right=mid+1<m ? mat[rowmax][mid+1]:-1;
+            if(mat[rowmax][mid]>left&&mat[rowmax][mid]>right) return {rowmax,mid};
+            else if(mat[rowmax][mid]<left) high=mid-1;
+            else low=mid+1;
+        }
+        return {-1,-1};
     }
 };
