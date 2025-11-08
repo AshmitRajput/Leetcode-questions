b445c940795711dbae067bedb50ca1f753268b60
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2025-11-08 17:56:00 +0530

    Time: 0 ms (100%), Space: 8 MB (26.1%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count=0;
+        int ans = start^goal;
+        for(int i=1;i<=31;i++){
+            if(ans&1==1) count++;
+            ans=ans>>1;
+        }
+        return count;
+    }
+};
