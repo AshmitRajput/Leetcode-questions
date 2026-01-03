f5c4d6aa14fe22bbe4277ee036d7c029cb51361d
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2026-01-03 15:09:06 +0530

    Time: 0 ms (100%), Space: 24.2 MB (99.22%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,47 +11,19 @@
  */
 class Solution {
 public:
-    void parentmap(TreeNode* root,unordered_map<TreeNode*,TreeNode*>& mpp){
-        if(!root)return;
-        if(root->left){
-            mpp[root->left]=root;
-            parentmap(root->left,mpp);
+    int ans=-1,cnt=0;
+    void inorder(TreeNode* root,int k){
+        if(!root) return;
+        inorder(root->left,k);
+        cnt++;
+        if(k==cnt){
+            ans=root->val;
+            return;
         }
-        if(root->right){
-            mpp[root->right]=root;
-            parentmap(root->right,mpp);
-        }
-        
+        inorder(root->right,k);
     }
     int kthSmallest(TreeNode* root, int k) {
-        unordered_map<TreeNode*,TreeNode*>mpp;
-        unordered_map<TreeNode*,int>vis;
-        mpp[root]=NULL;
-        parentmap(root,mpp);
-        int a=0;
-        while(root->left!=NULL){
-            root=root->left;
-        }
-        while(true){
-            vis[root]=1;
-            a++;
-            if(k==a) return root->val;
-            if (root->right && !vis[root->right]) {
-                root = root->right;
-                while (root->left) root = root->left;
-            }
-            else {
-                TreeNode* p = mpp[root];
-                while (p && vis[p]) {
-                    root = p;
-                    p = mpp[root];
-                }
-                root = p;
-            }
-
-        }
-        return -1;
-
-
+        inorder(root,k);
+        return ans;
     }
 };
