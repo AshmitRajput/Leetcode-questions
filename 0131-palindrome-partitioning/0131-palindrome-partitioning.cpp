a999513fc25356cbe0d71e3c9d0c8e94d71ec1bb
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2026-01-14 10:24:19 +0530

    Time: 66 ms (23.43%), Space: 61.6 MB (49.28%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<string>path;
+        vector<vector<string>>ans;
+        func(s,0,path,ans);
+        return ans;
+    }
+    void func(string s,int index,vector<string>&path,vector<vector<string>>&ans){
+        if(index==s.size()){
+            ans.push_back(path);
+            return;
+        }
+        for(int i=index;i<s.size();i++){
+            if(checkpalindrome(s,index,i)){
+                path.push_back(s.substr(index,i-index+1));
+                func(s,i+1,path,ans);
+                path.pop_back();
+
+            }
+        }
+    }
+    bool checkpalindrome(string s, int start,int end){
+        while(start<=end){
+        if(s[start++]!=s[end--]){
+            return false;
+        }
+       }
+        return true;
+    }
+};
