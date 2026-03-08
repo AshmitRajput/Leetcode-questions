99773a500143ed54fe6129bf30ee716b6b179b67
Ashmit Rajput <150168122+AshmitRajput@users.noreply.github.com>
2026-03-08 23:46:10 +0530

    Time: 4 ms (31.63%), Space: 8.5 MB (91.32%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0 || (x%10 == 0 && x != 0)){
+        int ans  = 0;
+        int temp = x;
+       
+        if(x<0){
             return false;
         }
-        int rev = 0;
-        int temp = x;
-        while(temp>0){
-            int digit = temp%10;
-            if((rev>INT_MAX/10) || (rev<INT_MIN/10)){
-                return false;
+        while( temp != 0) {
+            
+            int digit = temp % 10;
+            
+            if( (ans > INT_MAX/10) || (ans < INT_MIN/10)) {
+                return 0;
             }
-            rev = rev*10 + digit;
-            temp = temp/10;
+            
+            ans = (ans * 10) + digit;
+            temp = temp / 10;
+            
         }
-        if(rev == x){
+        
+        if(ans==x){
             return true;
         }
-        else{
-            return false;
-        }
+        else
+        return false;
     }
 };
